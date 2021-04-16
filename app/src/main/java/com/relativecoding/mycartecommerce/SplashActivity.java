aedb2cd01a157f155fd70076f628e51e361debd6
Ak <aks222345@gmail.com>
2021-04-16 13:15:48 +0530

    improved ui

---
+++ app/src/main/java/com/relativecoding/mycartecommerce/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.relativecoding.mycartecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+private static  int SPLASH_SCREEN_TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+         setContentView(R.layout.activity_splash);
+         getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+
+            public void run() {
+
+                Intent i=new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(i);
+
+                //invoke the SecondActivity.
+
+
+
+                finish();
+
+                //the current activity will get finished.
+
+            }
+
+        }, SPLASH_SCREEN_TIME_OUT);
+
+    }
+        }
